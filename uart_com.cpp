35330b3a7ec8a21f03fb533b91ebdca4b2fc816e
fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2024-11-05 15:34:12 +0900

    set ndc for no calibration
    Signed-off-by: fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- uart_com.cpp
+++ uart_com.cpp
@@ -134,6 +134,20 @@ bool uart_com::parse_sensi()
   }
 }
 
+bool uart_com::parse_ndc()
+{
+  if (2 == words_len &&
+    IsDecString(words[1]))
+  {
+    this->ndc = DecStringToDec(words[1]);
+    String logndcmsg = "expected ndc (" + String(this->ndc) +")";
+    ch_.loginfo(logndcmsg.c_str());
+    return true;
+  } else {
+    return false;
+  }
+}
+
 bool uart_com::parse_dat()
 {
   if (words_len != 12) {return false;}
@@ -285,6 +299,8 @@ void uart_com::StringCmdParse(char c)
       this->parse_thresh();
     } else if (strcmp(words[0], "SENSI") == 0) {
       this->parse_sensi();
+    } else if (strcmp(words[0], "NDC") == 0) {
+      this->parse_ndc();
     } else if (strcmp(words[0], "E") == 0) {
       this->parse_error();
     } else if (strcmp(words[0], "") == 0) {
@@ -314,6 +330,7 @@ void uart_com::update()
 void uart_com::start()
 {
   UART.println("start");
+  set_ndc(NEG_DELTA_CNT);
 }
 void uart_com::stop()
 {
@@ -409,6 +426,14 @@ bool uart_com::set_sensi(int sensi)
   return true;
 }
 
+bool uart_com::set_ndc(int ndc)
+{
+  String buf = "NDC,";
+  buf += String(ndc);
+  UART.println(buf);
+  return true;
+}
+
 bool uart_com::is_started()
 {
   return this->_started;
@@ -451,4 +476,3 @@ void uart_com::check_feedback()
   resync_l = 0;
   }
 }
-
